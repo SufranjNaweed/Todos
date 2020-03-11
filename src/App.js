e92aa84894c6f52400a6801c5d1bd2dbd15611b6
SufranjNaweed <sufranj.naweed@gmail.com>
2020-03-11 21:05:06 +0100

    fixing : component's name (ThemeToogle -> ThemeToggle)

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './index.css';
 import Navbar from './Components/Navbar';
 import BookList from './Components/BookList';
 import ThemeContextProvider from './Contexts/ThemeContext';
-import ThemeToggle from './Components/ThemeToogle';
+import ThemeToggle from './Components/ThemeToggle';
 import AuthContextProvider from './Contexts/AuthContext';
 
 function App() {

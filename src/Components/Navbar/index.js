a55c0f68402a9a71cf4b53ad6229849b257fae57
SufranjNaweed <sufranj.naweed@gmail.com>
2020-03-11 20:44:22 +0100

    Conversion Class base component to functionnal base component

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,41 +1,29 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import  {ThemeContext} from '../../Contexts/ThemeContext';
 import { AuthContext } from '../../Contexts/AuthContext';
 
-class Navbar extends Component {
-  render() { 
-    console.log(this.context)
-
-    return (
-      <AuthContext.Consumer>{(authContext) => (
-        <ThemeContext.Consumer>{(themecontext) => {
-          const { isAuthenticated, toggleAuth } = authContext;
-          const { isLightTheme, light, dark } = themecontext;
-          const theme = isLightTheme ? light : dark;
-          return(
-            <nav style={{ 
-              background : theme.bg, 
-              color : theme.color
-            }}
-            >
-              <h1>Context App</h1>
-              <div onClick={toggleAuth}>
-                {isAuthenticated ?  'Logged In' :  'Logged Out'}
-              </div>
-              <ul>
-                <li style={{background : theme.ui }}>Home</li>
-                <li style={{background : theme.ui }}>About</li>
-                <li style={{background : theme.ui }}>Contact</li>
-              </ul>
-            </nav>
-          )
-        }}
-        </ThemeContext.Consumer>
-      )}
-
-      </AuthContext.Consumer>
-    );
-  }
+const Navbar = () => {
+  const { isAuthenticated, toggleAuth } = useContext(AuthContext);
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const theme = isLightTheme ? light : dark;
+  return ( 
+    <nav style={{ 
+      background : theme.bg, 
+      color : theme.color
+    }}
+    >
+      <h1>Context App</h1>
+      <div onClick={toggleAuth}>
+        {isAuthenticated ?  'Logged In' :  'Logged Out'}
+      </div>
+      <ul>
+        <li style={{background : theme.ui }}>Home</li>
+        <li style={{background : theme.ui }}>About</li>
+        <li style={{background : theme.ui }}>Contact</li>
+      </ul>
+    </nav>
+   );
 }
  
 export default Navbar;
+ 
